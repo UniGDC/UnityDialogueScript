3aed91847848d445d41f2b8c706654501e616c6e
up2021 <up2021@ocplpatron.local>
2017-05-14 12:08:18 -0700

    Initial commit

---
+++ UnityDialogueScript/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: up2021
+ * Date: 5/14/2017
+ * Time: 12:03 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace UnityDialogueScript
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
